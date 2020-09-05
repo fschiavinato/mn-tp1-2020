aa8e05e627c4cd644e04ea234e48e09a78f80f85
fschiavinato <francocschiavinato@gmail.com>
2020-09-04 22:09:05 -0300

    casi funciona.

--- src/cmm.cpp
+++ src/cmm.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#include<vector>
 
 #define forn(i,n) for(int i=0;i<int(n);i++)
+#define dforn(i,n) for(int i=int(n)-1;i>=0;i--)
 #define forsn(i,s,n) for(int i=int(s);i<int(n);i++)
+#define dforsn(i,s,n) for(int i=int(n)-1;i>=int(s);i--)
 
 using namespace std;
 
@@ -28,18 +31,17 @@ class matriz {
         T* const operator[](int i) {
             return this->entradas[i];
         }
-
 };
 
-float* const cmm(int w[], int l[], matriz<int>& n, const int T);
-void eg(matriz<float>& C, float b[], const int T);
+vector<float> cmm(vector<int>& w, vector<int>& l, matriz<int>& n, const int T);
+vector<float> eg(matriz<float>& C, vector<float>& b, const int T);
 
 int main() {
     int T, P;
-    int w[T], l[T];
+    cin >> T >> P;
+    vector<int> w(T), l(T);
     matriz<int> n(T,T);
     int fecha, equipo1, equipo2, goles1, goles2;
-    cin >> T >> P;
     forn(i, P) {
         cin >> fecha >> equipo1 >> goles1 >> equipo2 >> goles2;
         if(goles1 > goles2) {
@@ -51,39 +53,47 @@ int main() {
             l[equipo1-1]++;
         }
         n[equipo1-1][equipo2-1]++;
+        n[equipo2-1][equipo1-1]++;
     }
-    float* r = cmm(w, l, n, T);
+    vector<float> r = cmm(w, l, n, T);
     forn(i, T) {
         cout << r[i] << endl;
     }
-    delete[] r;
 }
 
-float* const cmm(int w[], int l[], matriz<int>& n, const int T) {
+vector<float> cmm(vector<int>& w, vector<int>& l, matriz<int>& n, const int T) {
     matriz<float> C(T,T);
-    float* b = new float[T];
+    vector<float> b(T);
     forn(i, T) {
         b[i] = 1 + (w[i] - l[i]) / 2;
         forn(j, T) {
             if(i == j) {
-                C[i][j] = 2 + n[i][j];
+                C[i][j] = 2 + w[i] + l[i];
             }
             else {
                 C[i][j] = -n[i][j];
             }
         }
     }
-    eg(C, b, T);
-    return b;
+    return eg(C, b, T);
 }
 
-void eg(matriz<float>& C, float b[], const int T) {
+vector<float> eg(matriz<float>& C, vector<float>& b, const int T) {
+    vector<float> x(T);
     forn(i, T) {
-        forsn(j, i, T) {
-            forsn(k, j, T) {
-                C[j][k] = C[j][k] - C[i][k] * C[j][i] / C[i][i];
-                b[j] = b[j] - b[i] * C[j][i];
+        forsn(k, i+1, T) {
+            forsn(j, i, T) {
+                C[k][j] -= C[i][j] * C[k][i] / C[i][i];
+                b[k] -= b[i] * C[k][i] / C[i][i];
             }
         }
     }
+    dforn(i, T) {
+        x[i] = b[i];
+        dforsn(j, i+1, T) {
+            x[i] -= C[i][j] * x[j];
+        }
+        x[i] = x[i] / C[i][i];
+    }
+    return x;
 }
--- src/metnum.py
+++ src/metnum.py
@@ -11,11 +11,11 @@ def build():
 
 def compile():
   for source in sources:
-    run(compiler, '-std=c++11', '-O2', '-c', source+'.cpp', '-o', source+'.o')
+    run(compiler, '-std=c++11', '-g', '-O0', '-c', source+'.cpp', '-o', source+'.o')
 
 def link():
   objects = [s+'.o' for s in sources]
-  run(compiler, '-o', executable, objects)
+  run(compiler, '-g', '-o', executable, objects)
 
 def clean():
   autoclean()
